22f3ad297e6d5c424e3e4f5a913f203f2300d811
Chithra <47950208+chithradevim@users.noreply.github.com>
2022-06-11 22:07:20 +0530

    Update tutorial.service.ts

--- angular-12-client/src/app/services/tutorial.service.ts
+++ angular-12-client/src/app/services/tutorial.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
 const baseUrl = {
-  origin: process.env.ANGULAR_APP_API_BASE_URL || 'http://localhost:8082/api/tutorials';
+  origin: process.env.ANGULAR_APP_API_BASE_URL || 'http://localhost:8082/api/tutorials'
 };
 @Injectable({
   providedIn: 'root'
