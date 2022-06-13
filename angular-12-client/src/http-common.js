19f6b74253c89ec70b59a28a5b80637a8929acec
Chithra <47950208+chithradevim@users.noreply.github.com>
2022-06-13 17:32:56 +0530

    Update http-common.js

--- angular-12-client/src/http-common.js
+++ angular-12-client/src/http-common.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-const baseUrl = axios.create({
   baseURL: process.env.ANGULAR_APP_API_BASE_URL || 'http://localhost:8082/api/tutorials',
   headers: {
     "Content-type": "application/json"
